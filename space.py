860d3a15a2859ed32a4de5acf2d16d6557247e8d
ISAC AZEVEDO PINHEIRO <isacazevedo@id.uff.br>
2025-10-07 13:02:40 -0300

    Update space.py

--- space.py
+++ space.py
@@ -40,6 +40,9 @@ Dificil.set_position(janela.width/2 - Botao_jogar.width/2 - 300, janela.height/2
 Impossivel = Sprite("ranking.png")
 Impossivel.set_position(janela.width/2 - Botao_jogar.width/2 - 300, janela.height/2 - (distancia*3) + nova_dist)
 
+jogador = Sprite("jogador.png")
+jogador.set_position(janela.width/2 - jogador.width/2, janela.height*7/8)
+
 state = "string"
 
 #tapa = Sprite("tapa.png")
@@ -48,17 +51,26 @@ state = "string"
 branco = (255, 255, 255)
 preto = (0, 0, 0)
 
+vel_jog = 200
+
+#click
+mouse_livre = True
+
 while True:
     if state == "string":
         janela.set_background_color(preto)
-        if mouse.is_over_object(Botao_jogar) and mouse.is_button_pressed(1):
+        if mouse.is_over_object(Botao_jogar) and mouse.is_button_pressed(1) and mouse_livre:
             state = "jogando"
-        if mouse.is_over_object(Botao_dificuldade) and mouse.is_button_pressed(1):
+            mouse_livre =False
+        if mouse.is_over_object(Botao_dificuldade) and mouse.is_button_pressed(1) and mouse_livre:
             state = "dificuldades"
-        if mouse.is_over_object(Botao_rank) and mouse.is_button_pressed(1):
+            mouse_livre =False
+        if mouse.is_over_object(Botao_rank) and mouse.is_button_pressed(1) and mouse_livre:
             state = "rank"
-        if mouse.is_over_object(Botao_sair) and mouse.is_button_pressed(1):
+            mouse_livre =False
+        if mouse.is_over_object(Botao_sair) and mouse.is_button_pressed(1) and mouse_livre:
             state = "saiu"
+            mouse_livre =False
             
         Botao_jogar.draw()
         janela.draw_text("JOGAR", janela.width/2 - Botao_jogar.width/2 + 63, janela.height/2 + nova_dist, tam, preto, "Calibri", True)
@@ -75,10 +87,21 @@ while True:
         Titulo.draw()
     
     if state == "jogando":
-        janela.set_background_color((0, 0, 0))
+        janela.set_background_color(preto)
+        jogador.draw()
         janela.draw_text("JOGANDO", janela.width/2 - 120, 20, 50, branco, "Calibri", True)
         if teclado.key_pressed("esc"):
         	state = "string"
+        	
+        #barra esquerda controlado por W, S:
+        if teclado.key_pressed("RIGHT"):
+            jogador.x += vel_jog * janela.delta_time()
+        if teclado.key_pressed("LEFT"):
+            jogador.x -= vel_jog * janela.delta_time()
+
+	#colisão barras:
+	if jogador.x < 0:
+	    jogador.x = 0
 
 
     if state == "dificuldades":
@@ -114,9 +137,10 @@ while True:
         janela.draw_text("RANK", janela.width/2 - 80, 20, 50, branco, "Calibri", True)
         if teclado.key_pressed("esc"):
         	state = "string"
-
-
     
+    if not mouse.is_button_pressed(1):
+    	mouse_livre = True
+
     if state == "saiu":
         break
 
